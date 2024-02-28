e9312a200bd92c0a5bc8a7452c0bca4ac3c6947a
hasan basri ertekin <hasanbasriertekin96@gmail.com>
2024-02-28 14:43:27 +0300

    usersType added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import{ useEffect, useState } from "react";
+import { UsersType } from "../types/user";
+import Card from 'react-bootstrap/Card';
 
 const url = "https://jsonplaceholder.typicode.com/users";
 
 const Home = () => {
 
-    const [users, setUsers]= useState([]);
+    const [users, setUsers]= useState<UsersType>([]);
 
   const getUsers = async () => {
     const res = await fetch(url);
@@ -16,8 +18,20 @@ useEffect(()=>{
   getUsers();
 } );
 
-
-  return <div>Home</div>;
+  return <div>
+    {users.map((user) =>  
+    <Card key={user.id} style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{user.name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{user.username}</Card.Subtitle>
+        <Card.Text>
+        {user.email}
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>)}
+  </div>;
 };
 
 export default Home;
